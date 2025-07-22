e184625561a6dcb849d5a81dd2ec5fe3c4cff144
Aman kumar <amananshukumarpurnea@gmail.com>
2025-07-22 16:07:04 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
+    "https://mirror-video-call-backend.onrender.com" :
 
     "http://localhost:8000"
 
